94ae79c5153ea1e7c95534e9d7eec8a293e7279b
饶家俊 <raojiajun@bimwinner.com>
2019-11-20 23:02:48 +0800

    点击按钮, 播放对应视频

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -3,14 +3,18 @@ import { Player } from 'video-react';
 import "../../../node_modules/video-react/dist/video-react.css"; // import css
 
 const Video = (props)=>{
-	const {match} = props.match.params.id;
+	const id = props.match.params.id;
+
+	const computedVideoUrl = ()=>{
+		return `/${id}.mov`;
+	};
+
 	return (
 		<div>
-			{match}
 			<Player
 				playsInline
 				poster="/assets/poster.png"
-				src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+				src={computedVideoUrl()}
 			/>
 		</div>
 	);
--- src/pages/videoList/VideoList.jsx
+++ src/pages/videoList/VideoList.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Button} from 'antd-mobile';
+import {withRouter} from 'react-router-dom';
 
-const Video = ()=>{
+const Video = (props)=>{
 	return (
 		<div>
-			<Button>第一期视频</Button>
-			<Button>第二期视频</Button>
-			<Button>第三期视频</Button>
-			<Button>第四期视频</Button>
+			<Button onClick={()=>{props.history.push('/video/1')}}>第一期视频</Button>
+			<Button onClick={()=>{props.history.push('/video/2')}} disabled>第二期视频</Button>
+			{/*<Button onClick={()=>{props.history.push('/video/3')}}>第三期视频</Button>*/}
+			{/*<Button onClick={()=>{props.history.push('/video/4')}}>第四期视频</Button>*/}
 		</div>
 	);
 };
 
 
-export default Video;
+export default withRouter(Video);
