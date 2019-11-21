e83fce4751cc8f72a1c81aff9592cd3efe03ce71
饶家俊 <raojiajun@bimwinner.com>
2019-11-21 17:51:36 +0800

    info form

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -1,13 +1,24 @@
+import { List, InputItem } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
 import React from 'react';
 
-const Info = ()=>{
+const Info = (props)=>{
+	const {getFieldProps} = props.form;
 	return (
 		<div>
-			FormCollect
+			<List renderHeader={() => 'Show clear'}>
+				<InputItem
+					{...getFieldProps('inputclear')}
+					clear
+					placeholder="displayed clear while typing"
+				>标题</InputItem>
+			</List>
 		</div>
 	);
 };
 
+const InfoWrapper = createForm()(Info);
 
-export default Info;
+
+export default InfoWrapper;
