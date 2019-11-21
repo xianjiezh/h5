80b256e299b087e852b9abf5a9ed014175ba34b8
饶家俊 <raojiajun@bimwinner.com>
2019-11-21 22:50:43 +0800

    4 space => 2space

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,109 +1,109 @@
 var quizQuestions = [
-  {
-      question: "最常见的亚健康表现是?",
-      answers: [
-          {
-              type: "11",
-              content: "尿酸高"
-          },
-          {
-              type: "12",
-              content: "体脂高"
-          },
-          {
-              type: "13",
-              content: "脂肪肝"
-          },
-          {
-              type: "14",
-              content: "以上都是"
-          }
-      ]
-  },
-  {
-      question: "Burpee（波比）跳的动作要领：起身跳高，______击掌？",
-      answers: [
-          {
-              type: "21",
-              content: "双手"
-          },
-          {
-              type: "22",
-              content: "用力"
-          },
-          {
-              type: "23",
-              content: "头顶"
-          },
-          {
-              type: "24",
-              content: "身后",
-          }
-      ]
-  },
-  {
-      question: "关于669的名称解释正确的是?",
-      answers: [
-          {
-              type: "31",
-              content: "6种动作"
-          },
-          {
-              type: "32",
-              content: "每种做6组"
-          },
-          {
-              type: "33",
-              content: "每组做9次"
-          },
-          {
-              type: "34",
-              content: "以上都是"
-          },
-      ]
-  },
-  {
-      question: "以下食物中属于低碳低脂的是?",
-      answers: [
-          {
-              type: "41",
-              content: "可乐"
-          },
-          {
-              type: "42",
-              content: "玉米"
-          },
-          {
-              type: "43",
-              content: "薯条"
-          },
-          {
-              type: "44",
-              content: "面条"
-          },
-      ]
-  },
-  {
-      question: "下面答案中，关于登山者的动作要领描述错误的是?",
-      answers: [
-          {
-              type: "51",
-              content: "手臂支撑与地面垂直"
-          },
-          {
-              type: "52",
-              content: "身体呈一条直线"
-          },
-          {
-              type: "53",
-              content: "双手交替支撑"
-          },
-          {
-              type: "54",
-              content: "双腿交替提膝"
-          },
-      ]
-  }
+	{
+		question: '最常见的亚健康表现是?',
+		answers: [
+			{
+				type: '11',
+				content: '尿酸高',
+			},
+			{
+				type: '12',
+				content: '体脂高',
+			},
+			{
+				type: '13',
+				content: '脂肪肝',
+			},
+			{
+				type: '14',
+				content: '以上都是',
+			},
+		],
+	},
+	{
+		question: 'Burpee（波比）跳的动作要领：起身跳高，______击掌？',
+		answers: [
+			{
+				type: '21',
+				content: '双手',
+			},
+			{
+				type: '22',
+				content: '用力',
+			},
+			{
+				type: '23',
+				content: '头顶',
+			},
+			{
+				type: '24',
+				content: '身后',
+			},
+		],
+	},
+	{
+		question: '关于669的名称解释正确的是?',
+		answers: [
+			{
+				type: '31',
+				content: '6种动作',
+			},
+			{
+				type: '32',
+				content: '每种做6组',
+			},
+			{
+				type: '33',
+				content: '每组做9次',
+			},
+			{
+				type: '34',
+				content: '以上都是',
+			},
+		],
+	},
+	{
+		question: '以下食物中属于低碳低脂的是?',
+		answers: [
+			{
+				type: '41',
+				content: '可乐',
+			},
+			{
+				type: '42',
+				content: '玉米',
+			},
+			{
+				type: '43',
+				content: '薯条',
+			},
+			{
+				type: '44',
+				content: '面条',
+			},
+		],
+	},
+	{
+		question: '下面答案中，关于登山者的动作要领描述错误的是?',
+		answers: [
+			{
+				type: '51',
+				content: '手臂支撑与地面垂直',
+			},
+			{
+				type: '52',
+				content: '身体呈一条直线',
+			},
+			{
+				type: '53',
+				content: '双手交替支撑',
+			},
+			{
+				type: '54',
+				content: '双腿交替提膝',
+			},
+		],
+	},
 ];
 
 export default quizQuestions;
